0a1a1d601dc197fc745b8364bfbe4001d7d7a46c
alikemal.ocalan <alikemal.ocalan@trendyol.com>
2020-02-09 00:56:38 +0300

    Fix page param default

--- src/main/scala/com/github/alikemalocalan/controller/TweetController.scala
+++ src/main/scala/com/github/alikemalocalan/controller/TweetController.scala
@@ -1,5 +1,7 @@
 package com.github.alikemalocalan.controller
 
+import java.util.Optional
+
 import com.github.alikemalocalan.services.TwitterServices
 import org.json4s._
 import org.json4s.jackson.Serialization._
@@ -12,9 +14,9 @@ import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, R
 class TweetController {
   implicit val formats: DefaultFormats.type = DefaultFormats
 
-  @RequestMapping(path = Array("/tweets"), method = Array(RequestMethod.GET), produces = Array(MediaType.APPLICATION_JSON_VALUE), params = Array("page"))
+  @RequestMapping(path = Array("/tweets"), method = Array(RequestMethod.GET), produces = Array(MediaType.APPLICATION_JSON_VALUE))
   @ResponseBody
-  def handleRequest(@RequestParam page: String = "50"): String =
-    write(TwitterServices.getFavorites(maxTweetNumber = page.toInt))
+  def handleRequest(@RequestParam(value = "50", required = false) page: Optional[String]): String =
+    write(TwitterServices.getFavorites(maxTweetNumber = page.orElse("50").toInt))
 
 }
