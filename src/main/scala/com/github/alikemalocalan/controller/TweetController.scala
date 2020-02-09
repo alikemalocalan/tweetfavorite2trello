df5dd8dcd3088e75444dc4b2bc49dbfd85a7160a
alikemal.ocalan <alikemal.ocalan@trendyol.com>
2020-02-09 23:50:33 +0300

    Fix optional params

--- src/main/scala/com/github/alikemalocalan/controller/TweetController.scala
+++ src/main/scala/com/github/alikemalocalan/controller/TweetController.scala
@@ -15,11 +15,10 @@ class TweetController {
   implicit val formats: DefaultFormats.type = DefaultFormats
 
   @GetMapping(path = Array("/tweets"),
-    produces = Array(MediaType.APPLICATION_JSON_VALUE),
-    params = Array("page", "count"))
+    produces = Array(MediaType.APPLICATION_JSON_VALUE))
   @ResponseBody
-  def handleRequest(@RequestParam(name = "page", defaultValue = "1", required = false) page: Optional[String],
-                    @RequestParam(name = "count", defaultValue = "50", required = false) count: Optional[String]): String =
+  def handleRequest(@RequestParam(defaultValue = "1", required = false) page: Optional[String],
+                    @RequestParam(defaultValue = "50", required = false) count: Optional[String]): String =
     write(TwitterServices.getFavorites(page = page.orElse("1").toInt, maxTweetNumber = count.orElse("50").toInt))
 
 }
